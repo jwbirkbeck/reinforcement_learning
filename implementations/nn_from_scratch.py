ef185dea53ade788e9c19757e5921d6c7b73364b
jwb110213 <29716383+JWB110123@users.noreply.github.com>
2020-09-01 22:06:45 +0100

    adding updated raw NN toy code

--- implementations/nn_from_scratch.py
+++ implementations/nn_from_scratch.py
@@ -12,43 +12,72 @@ X = np.array([[0,0,0,1],
               [0,0,1,0],
               [0,1,0,0],
               [1,0,0,0]])
-Y = np.array([0,1,0,1])
+Y = np.array([[0,1],[1,0],[0,1],[1,0]])
 
 # A correct NN would see that a '1' in the first or third input corresponds with an output value of 0, and similarly the second
 # or fourth input corresponds with an output of 1.
 
-# The design of the neural network will be a 4, 4, 1. This is more complicated than it needs to be (we could do without
+# The design of the neural network will be a in 4, 4, 1. This is more complicated than it needs to be (we could do without
 # the hidden layer and still train to 100% accuracy) but it tests the backprop algo is correct. Between the hidden layer
 # and the output layer I will have a sigmoid activation layer
 
 # With this design of NN, we need a weights matrix which is 4x4 between the input layer and the hidden layer and a
 # length 4 vector for weights between the hidden layer and the output layer (which has length 1).
 
-class NN:
-    def __init__(self):
-        # This is using the simple initialisation from the module notes - N(0,0.1) as small random weights. The lecture notes mention He
-        # and Xavier initalisations shortly after this method but that's beyond the requirements of this example.
-        self.W1 = np.random.normal(0,0.1,16).reshape((4,4))
-        self.w2 = np.random.normal(0,0.1,4)
-        self.b1 = np.zeros(4)
-        self.b2 = np.zeros(1)
-        self.learning_rate = 0.01
+W1 = np.random.normal(0,0.1,16).reshape((4,4))
+W2 = np.random.normal(0,0.1,8).reshape((4,2))
 
-    def acti(self, x):
-        return 1 / (1 + np.exp(-x))
+learning_rate = 0.01
+x_vec = X[0]
 
-    def predict(self, x_vec):
-        # Matrix multiply input observation by W1 to get z. Then put z through activation.
-        Z1 = np.matmul(self.W1, x_vec) + self.b1
-        H1 = self.acti(Z1)
-        z2 = np.matmul(self.w2, H1) + self.b2
-        prediction = self.acti(z2)
-        return prediction
 
-    def backprop(self, x_vec, y):
-        prediction = self.predict(x_vec)
-        # TODO: complete the below.  We don't want to actually return the logloss, we need to calculate the partial
-        #  diff of the logloss w.r.t the weights and then update the weights based on how much they contribute to the
-        #  overall logloss.
-        logloss = -( (1 - y) * np.log(1 - prediction) + y * np.log(prediction))
-        return(logloss)
+def acti(x):
+    return 1 / (1 + np.exp(-x))
+
+def deriv_acti(x):
+    return x * (1-x)
+
+def predict(x_vec):
+    # Matrix multiply input observation by W1 to get z. Then put z through activation.
+    Z1 = np.matmul(x_vec, W1)
+    H1 = acti(Z1)
+    Z2 = np.matmul(H1, W2)
+    H2 = acti(Z2)
+    prediction = acti(H2)
+    return prediction
+
+    # def backprop(self, x_vec, y):
+    #     prediction = self.predict(x_vec)
+    #     # TODO: complete the below.  We don't want to actually return the logloss, we need to calculate the partial
+    #     #  diff of the logloss w.r.t the weights and then update the weights based on how much they contribute to the
+    #     #  overall logloss.
+    #
+    #     # * find the error between the network's predictions and the outputs
+    #     # * apply the derivative of the activation function (delta output sum)
+    #     # * use delta output sum to work out how much error z2 contributed - we do this
+    #     # with a dot product
+    #     # * do same steps for previous layer
+    #     error =  prediction - y
+    #     deriv_error = self.deriv_acti(error)
+    #     # Incorporate the learning rate:
+    #     deriv_error *= self.learning_rate
+    #
+    #     second_layer_deriv_err = deriv_error.dot
+    #     second_layer_delta_output_sum = second_layer_error.dot
+    #     return(logloss)
+
+
+prediction = predict(X[0])
+y = Y[0]
+
+o_error =  y - prediction
+o_delta = o_error * deriv_acti(prediction)
+# Incorporate the learning rate:
+# deriv_error *= learning_rate
+
+z2_error = o_delta.dot(W2.T)
+z2_delta = z2_error * deriv_acti(Z1)
+
+z1_error = z2_delta.dot(W1)
+
+W1 += X[0]

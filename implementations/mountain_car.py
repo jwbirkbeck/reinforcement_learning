6c2e8495879734720663fb4c0b78441921444f52
jwb110213 <29716383+JWB110123@users.noreply.github.com>
2020-09-02 19:05:18 +0100

    get started on training mountaincar

--- implementations/mountain_car.py
+++ implementations/mountain_car.py
@@ -2,7 +2,7 @@ from classes.QLearnAgent import QLearnAgent
 from classes.MountainCar import MountainCar
 from tensorflow.keras.backend import clear_session
 from tensorflow.keras.models import load_model
-import gc
+
 clear_session()
 
 agent = QLearnAgent(game = MountainCar(),
@@ -13,33 +13,15 @@ agent = QLearnAgent(game = MountainCar(),
 
 # to avoid the memory leak currently in keras, we'll have to train in groups of 10k before saving, restarting the keras
 # session and loading the model back in:
-while agent.winstreak < 25:
-    agent.play_games(1, verbose=True)
-    agent.batch_train(32, verbose=False)
-    if agent._game_counter % 5000 == 0:
-        agent.brain.model.save('mountaincar.model')
-
-
-
-agent.brain.model.save('mountaincar.model')
-clear_session()
 agent.brain.model = load_model('mountaincar.model')
 
 
-agent.display_gameplay()
-
-# Alternatively, we can teach the neural net by playing the game ourselves a few times:
-agent.memory.wipe()
-agent.human_game()
-# since the learning rate is low for the neural net, but we want the network to learn a lot from us, we can
-# batch learn multiple times on our game. It would be better to play a few different games and monitor the performance
-# of the neural net but this hacky approach works for now.
-for _ in range(1000):
-    agent.batch_train(32, verbose=True)
-
-agent.display_gameplay()
-
-
-agent.game.reset_env()
-while True:
+while agent.winstreak < 25:
+    agent.play_games(1, verbose=True)
     agent.batch_train(32, verbose=False)
+    if agent._game_counter % 100 == 0:
+        agent.brain.model.save('mountaincar.model')
+        print("model saved!")
+
+
+agent.display_gameplay()
